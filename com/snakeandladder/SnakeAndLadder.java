d3a569100bb4959a74c8010d81481427690f63d5
rehan7594 <rehansdm94@gmail.com>
2021-04-16 01:16:18 -0700

    Added: Player gets to exact winning psition 100

--- com/snakeandladder/SnakeAndLadder.java
+++ com/snakeandladder/SnakeAndLadder.java
@@ -1,8 +1,10 @@
 package com.snakeandladder;
 
-public class SnakeAndLadder {
+public class SnakeAndLadder
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// variables
 		final int NoPlay = 0;
 		final int Ladder = 1;
@@ -10,7 +12,8 @@ public class SnakeAndLadder {
 		int PlayerPostion = 0; // initial position of player
 
 		// loop till player reach the winning spot which is 100
-		while (PlayerPostion < 100) {
+		while (PlayerPostion < 100)
+		{
 
 			// dice number generator
 			int die = (int) (Math.random() * 6) + 1;
@@ -20,30 +23,38 @@ public class SnakeAndLadder {
 			int option = (int) Math.floor(Math.random() * 10) % 3;
 			System.out.println("Genrat Random Postion :" + option);
 
-			if (option == 1) {
-				PlayerPostion = PlayerPostion + die;
+			if (option == 1) 
+			{
+				if (PlayerPostion <= 100 && (PlayerPostion + die <= 100))
+					PlayerPostion = PlayerPostion + die;
 				System.out.println("Your status is Ladder");
+
 				// printing position of player
 				System.out.println("Your Postion is :" + PlayerPostion);
+			}
 
-			} 
-			else if (option == 2) {
+			else if (option == 2)
+			{
 				// In case the player position moves below 0, then the player restarts from 0
-				if (PlayerPostion > 0 && (PlayerPostion - die) >= 0) {
+				if (PlayerPostion > 0 && (PlayerPostion - die) >= 0)
+				{
 					PlayerPostion = PlayerPostion - die;
 					System.out.println(" Your status is Snake");
 					System.out.println("Your Postion is :" + PlayerPostion);
 				}
 			}
-			else {
+
+			else 
+			{
 				System.out.println("Your Status is No Play ");
 				System.out.println("Your Postion is :" + PlayerPostion);
 			}
 		}
-
 	}
 
-
 }
+
+
+
 
 
