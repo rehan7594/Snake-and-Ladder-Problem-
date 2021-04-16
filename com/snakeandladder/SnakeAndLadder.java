366e85ff21d2b73446cd10d791d54c3d4c708518
rehan7594 <rehansdm94@gmail.com>
2021-04-15 22:53:29 -0700

    Added: Single player position

--- com/snakeandladder/SnakeAndLadder.java
+++ com/snakeandladder/SnakeAndLadder.java
@@ -3,7 +3,8 @@ package com.snakeandladder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Game");
+		//Single player position
+		System.out.println("Starting position is zero");
 	}
 
 }
