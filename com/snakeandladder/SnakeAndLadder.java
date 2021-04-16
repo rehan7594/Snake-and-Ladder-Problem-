b5510c918158a775ac4f0d9016bf537a76ad3ff4
rehan7594 <rehansdm94@gmail.com>
2021-04-16 00:42:32 -0700

    Added: Player position and status

--- com/snakeandladder/SnakeAndLadder.java
+++ com/snakeandladder/SnakeAndLadder.java
@@ -3,9 +3,36 @@ package com.snakeandladder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		//when a player rolls the die
-		int output=(int)(Math.random() * 6) + 1;
-		System.out.println("The dice number is: "+ output);
+		// variables
+		final int NoPlay = 0;
+		final int Ladder = 1;
+		final int Snake = 2;
+		int PlayerPostion = 0;
+
+		// dice number generator
+		int die = (int) (Math.random() * 6) + 1;
+		System.out.println("The Dice Number is :" + die);
+
+		// option Number generator
+		int option = (int) Math.floor(Math.random() * 10) % 3;
+
+		if (option == 1) {
+			PlayerPostion = PlayerPostion + die;
+			System.out.println("Your status is Ladder");
+			System.out.println("Your Postion is :" + PlayerPostion);
+		} else if (option == 2) {
+
+			PlayerPostion = PlayerPostion - die;
+			System.out.println(" Your status is Snake");
+			System.out.println("Your Postion is :" + PlayerPostion);
+
+		} else {
+			System.out.println("Your Status is No Play :");
+			System.out.println("Your Postion is :" + PlayerPostion);
+		}
 	}
 
+
 }
+
+
