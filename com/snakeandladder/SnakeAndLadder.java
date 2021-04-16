f7a51cfcdeb60dd8fa5740cc319470af79d23108
rehan7594 <rehansdm94@gmail.com>
2021-04-15 23:42:57 -0700

    Added: Player rolls the die to get a number

--- com/snakeandladder/SnakeAndLadder.java
+++ com/snakeandladder/SnakeAndLadder.java
@@ -3,8 +3,9 @@ package com.snakeandladder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		//Single player position
-		System.out.println("Starting position is zero");
+		//when a player rolls the die
+		int output=(int)(Math.random() * 6) + 1;
+		System.out.println("The dice number is: "+ output);
 	}
 
 }
